8639dd6b338bcdd8a04da475fcd342a2b339dc1a
lunaticom <107802284+lunaticom@users.noreply.github.com>
2025-06-24 21:53:55 +0200

    Update compile.js

--- api/compile.js
+++ api/compile.js
@@ -19,7 +19,8 @@ function autoFormatSmart(text) {
 module.exports = async (req, res) => {
   const data = req.body;
   
-  const selectedTemplate = data.Template || data.template || "template";
+  const selectedTemplate = data.Template || "template";
+
 //console.log("DEBUG — Payload received:", data);
 //console.log("DEBUG — Selected template:", selectedTemplate);
 
